9d4c86949e0f0480177bd399e0b2bd1f45cce806
zowens24 <zowens83@gmail.com>
2020-11-02 14:54:11 -0500

    adds comedy functionality

--- js/script.js
+++ js/script.js
@@ -7,8 +7,8 @@
 const RO_URL = ['https://www.omdbapi.com/?apikey=364a927d&t=titanic&plot=full', 'https://www.omdbapi.com/?apikey=364a927d&t=the+notebook&plot=full', 'https://www.omdbapi.com/?apikey=364a927d&t=ghost&plot=full', 'https://www.omdbapi.com/?apikey=364a927d&t=call+me+by+your+name&plot=full', 'https://www.omdbapi.com/?apikey=364a927d&t=casablanca&plot=full', 'https://www.omdbapi.com/?apikey=364a927d&t=moonstruck&plot=full', 'https://www.omdbapi.com/?apikey=364a927d&t=dirty+dancing&plot=full', 'https://www.omdbapi.com/?apikey=364a927d&t=a+star+is+born&plot=full', 'https://www.omdbapi.com/?apikey=364a927d&t=brokeback+mountain&plot=full', 'https://www.omdbapi.com/?apikey=364a927d&t=an+officer+and+a+gentleman&plot=full', 'https://www.omdbapi.com/?apikey=364a927d&t=roman+holiday&plot=full', 'https://www.omdbapi.com/?apikey=364a927d&t=love+story&plot=full', 'https://www.omdbapi.com/?apikey=364a927d&t=gone+with+the+wind&plot=full', 'https://www.omdbapi.com/?apikey=364a927d&t=carol&plot=full']
 
 //comedy movies
-const CO_URL = []
-//const rand_co_movie = CO_URL[(Math.random() * CO_URL.length) | 0]
+const CO_URL = ['https://www.omdbapi.com/?apikey=364a927d&t=the+big+lebowski&plot=full', 'https://www.omdbapi.com/?apikey=364a927d&t=some+like+it+hot&plot=full', 'https://www.omdbapi.com/?apikey=364a927d&t=this+is+spinal+tap&plot=full', 'https://www.omdbapi.com/?apikey=364a927d&t=airplane&plot=full']
+
 //action movies
 const AC_URL = []
 const rand_ac_movie = AC_URL[(Math.random() * AC_URL.length) | 0]
@@ -43,6 +43,7 @@ const rand_rand_movie = RAND_URL[(Math.random() * RAND_URL.length) | 0]
 // State Data
 let romanceMovieData;
 let comedyMovieData;
+
 // Cached Element References 
 const $romance = $('#romance');
 const $comedy = $('#comedy');
@@ -60,7 +61,7 @@ const $random = $('#random');
 
 // Attached Event Listeners
 $romance.on('click', handleClickR);
-
+$comedy.on('click', handleClickC)
 // Functions
 function handleClickR() {
     //fetch data using AJAX 
@@ -71,8 +72,24 @@ function handleClickR() {
         // take the returned data and assign it to a global vairable
         romanceMovieData = data;
         //call render and tell the function that it needs to display a modal
-       render(true);
-       console.log(data);
+        render(true);
+      function render(showModal) {
+          if (showModal === true) {
+        const $RoModalContent = $(`<h5>${romanceMovieData.Title}</h5><br>
+        <img src="${romanceMovieData.Poster}"/><br>
+        <p>Year: ${romanceMovieData.Year} </p><br>
+        <p>Rated: ${romanceMovieData.Rated} </p><br>
+        <p>Runtime: ${romanceMovieData.Runtime} </p><br>
+        <p>Director: ${romanceMovieData.Director} </p><br>
+        <p>Writer: ${romanceMovieData.Writer} </p><br>
+        <p>Actors: ${romanceMovieData.Actors} </p><br>
+        <p>Plot: ${romanceMovieData.Plot} </p>`);
+        const $RoModal = $('#moviemodalR')
+     $RoModal.html($RoModalContent)
+     $RoModal.modal();
+          }
+      }
+       console.log(data)
         //call render to visualize it to the DOM
         //render();
 
@@ -81,24 +98,49 @@ function handleClickR() {
     });
 }
 
-    function render(showRomanceModal) {
-     if(showRomanceModal === true) {
-         //show modal 
-         //generate the html for the inner content for the modal
-         //call the modal function on the modal element
-         const $RoModalContent = $(`<h5>${romanceMovieData.Title}</h5><br>
-            <img src="${romanceMovieData.Poster}"/><br>
-            <p>Year: ${romanceMovieData.Year} </p><br>
-            <p>Rated: ${romanceMovieData.Rated} </p><br>
-            <p>Runtime: ${romanceMovieData.Runtime} </p><br>
-            <p>Director: ${romanceMovieData.Director} </p><br>
-            <p>Writer: ${romanceMovieData.Writer} </p><br>
-            <p>Actors: ${romanceMovieData.Actors} </p><br>
-            <p>Plot: ${romanceMovieData.Plot} </p>`);
-         const $RoModal = $('#moviemodalR')
-         $RoModal.html($RoModalContent)
-         $RoModal.modal();
-     }  
-     
+   
+    //show modal 
+
+    //generate the html for the inner content for the modal
+    //call the modal function on the modal element
+   
+    
+    function handleClickC() {
+        //fetch data using AJAX 
+        
+        const rand_co_movie = CO_URL[(Math.random() * CO_URL.length) | 0]
+        
+        $.ajax(rand_co_movie).then(function (data) {
+            // take the returned data and assign it to a global vairable
+            comedyMovieData = data;
+            //call render and tell the function that it needs to display a modal
+            render(true);
+          function render(showModal) {
+              if (showModal === true) {
+            const $CoModalContent = $(`<h5>${comedyMovieData.Title}</h5><br>
+            <img src="${comedyMovieData.Poster}"/><br>
+            <p>Year: ${comedyMovieData.Year} </p><br>
+            <p>Rated: ${comedyMovieData.Rated} </p><br>
+            <p>Runtime: ${comedyMovieData.Runtime} </p><br>
+            <p>Director: ${comedyMovieData.Director} </p><br>
+            <p>Writer: ${comedyMovieData.Writer} </p><br>
+            <p>Actors: ${comedyMovieData.Actors} </p><br>
+            <p>Plot: ${comedyMovieData.Plot} </p>`);
+            const $CoModal = $('#moviemodalC')
+         $CoModal.html($CoModalContent)
+         $CoModal.modal();
+              }
+          }
+           console.log(data);
+            //call render to visualize it to the DOM
+            //render();
+    
+        }, function (error) {
+            console.log('error: ', error);
+        });
     }
     
+
+
+
+
